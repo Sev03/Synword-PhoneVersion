b184cc1045473836952bbb6d810dbb578e511d34
Sev03 <anel.j03@gmail.com>
2016-04-11 12:41:02 +0200

    startupscreen mit internet connectivity check
    TODO:
    punkte (timed modus)
    abbruch bei zeit aus

--- app/src/main/java/com/example/anel/MainActivity.java
+++ app/src/main/java/com/example/anel/MainActivity.java
@@ -17,8 +17,8 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
-    }
 
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
